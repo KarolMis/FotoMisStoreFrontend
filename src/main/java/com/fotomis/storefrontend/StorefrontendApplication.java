f16240a13296301d7311744627bbff19afd3b0da
KarolMis <sunziboks@gmail.com>
2018-03-22 11:14:40 +0100

    Initial commit

---
+++ src/main/java/com/fotomis/storefrontend/StorefrontendApplication.java
@@ -0,0 +1,12 @@
+package com.fotomis.storefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorefrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorefrontendApplication.class, args);
+	}
+}
