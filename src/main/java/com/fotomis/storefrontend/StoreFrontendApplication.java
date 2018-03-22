71181f6744803924766ac6859bb9bcd1a1161d85
KarolMis <sunziboks@gmail.com>
2018-03-22 17:17:04 +0100

    spring security configured

--- src/main/java/com/fotomis/storefrontend/StoreFrontendApplication.java
+++ src/main/java/com/fotomis/storefrontend/StoreFrontendApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StorefrontendApplication {
+public class StoreFrontendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StorefrontendApplication.class, args);
+		SpringApplication.run(StoreFrontendApplication.class, args);
 	}
 }
---
+++ src/main/java/com/fotomis/storefrontend/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.fotomis.storefrontend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/index")
+                .permitAll()
+                .antMatchers("/sample")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
