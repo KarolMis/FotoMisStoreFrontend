75fe6d9a6cf29311d914efa6d46fc8d3fdc984f1
KarolMis <sunziboks@gmail.com>
2018-03-26 19:50:32 +0200

    bug fixed

---
+++ src/main/java/com/fotomis/storefrontend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.fotomis.storefrontend.repository;
+
+import com.fotomis.storefrontend.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+    User findByUsername(String username);
+}
---
+++ src/main/java/com/fotomis/storefrontend/service/UserSecurityService.java
@@ -0,0 +1,29 @@
+package com.fotomis.storefrontend.service;
+
+import com.fotomis.storefrontend.domain.User;
+import com.fotomis.storefrontend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.xml.ws.Action;
+
+@Service
+public class UserSecurityService implements UserDetailsService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("username not found");
+        }
+
+        return user;
+    }
+}
